4a54f62c05a32248b90a1f120bf3d0d4f86de50a
Ed Kang <edk926@g.harvard.edu>
2018-09-13 15:56:52 -0400

    start

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/***************************
+*name: Edward Kang
+*CSCIE-55 HW 0
+*date: 9/8/2018
+***************************/
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+
+}
